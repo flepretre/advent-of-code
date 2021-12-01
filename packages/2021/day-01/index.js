36b382ebc3f28899d5f142b15e098cc2a538cc0c
Florent Lepretre <florent.lepretre@gmail.com>
2021-12-01 07:32:39 +0100

    first day - step 1

--- packages/2021/day-01/index.js
+++ packages/2021/day-01/index.js
@@ -7,6 +7,32 @@ const run = async () => {
   const values = await read("./data.txt", "utf8").then((data) =>
     data.split("\n")
   );
+
+  let previousValue = null;
+  let increaseNb = 0;
+  values.forEach((value) => {
+    const current = parseInt(value);
+    if (previousValue) {
+      if (parseInt(current) > previousValue) {
+        increaseNb++;
+        console.log(
+          `${current} (increased) ${increaseNb} (${current} > ${previousValue})`
+        );
+      } else if (parseInt(current) === previousValue) {
+        console.log(
+          `${current} (same) ${increaseNb} (${current} > ${previousValue})`
+        );
+      } else {
+        console.log(
+          `${current} (decreased) ${increaseNb} (${current} <= ${previousValue})`
+        );
+      }
+    } else {
+      console.log("(N/A - no previous measurement)");
+    }
+    previousValue = current;
+  });
+  console.log(`total of ${increaseNb} increased`);
 };
 
 run();
