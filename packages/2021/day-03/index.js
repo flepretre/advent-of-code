11927dee43ea098f512f598cc8e665bbdcfd08fa
Florent Lepretre <florent.lepretre@gmail.com>
2021-12-03 10:40:04 +0100

    day 3

--- packages/2021/day-03/index.js
+++ packages/2021/day-03/index.js
@@ -3,14 +3,40 @@ const util = require("util");
 
 const read = util.promisify(fs.readFile);
 
-const stepOne = (values) => {};
+const reorder = (values) =>
+  values.reduce((acc, v) => {
+    for (let i = 0; i < v.length; i++) {
+      const c = parseInt(v[i]);
+      if (typeof acc[i] === "undefined") {
+        acc[i] = {
+          0: 0,
+          1: 0,
+        };
+      }
+      acc[i][c]++;
+    }
+    return acc;
+  }, []);
+
+const stepOne = (values) => {
+  const { gama, epsilon } = values.reduce(
+    (acc, v) => ({
+      gama: acc.gama + (v["0"] > v["1"] ? "0" : "1"),
+      epsilon: acc.epsilon + (v["0"] > v["1"] ? "1" : "0"),
+    }),
+    { gama: "", epsilon: "" }
+  );
+  console.log(gama, epsilon);
+
+  return parseInt(gama, 2) * parseInt(epsilon, 2);
+};
 
 const example = async () => {
-  const values = await read("./data.txt", "utf8").then((data) =>
+  const values = await read("./example.txt", "utf8").then((data) =>
     data.split("\n")
   );
 
-  const result = stepOne(values);
+  const result = stepOne(reorder(values));
   console.log("example result =", result);
 };
 
@@ -19,7 +45,7 @@ const run = async () => {
     data.split("\n")
   );
 
-  const result = stepOne(values);
+  const result = stepOne(reorder(values));
   console.log(`result =`, result);
 };
 
--- packages/2021/day-03/part2.js
+++ packages/2021/day-03/part2.js
@@ -3,15 +3,55 @@ const util = require("util");
 
 const read = util.promisify(fs.readFile);
 
-const stepOne = (values) => {};
+const reorder = (values) =>
+  values.reduce((acc, v) => {
+    for (let i = 0; i < v.length; i++) {
+      const c = parseInt(v[i]);
+      if (typeof acc[i] === "undefined") {
+        acc[i] = {
+          0: 0,
+          1: 0,
+        };
+      }
+      acc[i][c]++;
+    }
+    return acc;
+  }, []);
+
+const filter = (values, value, i) =>
+  values.filter((v) => {
+    console.log("v[0]", v[i], v);
+    return v[i] == value;
+  });
+
+const process = (values, compute) => {
+  let filtered = values;
+  let i = 0;
+  while (filtered.length > 1 && i < filtered[0].length) {
+    const ordered = reorder(filtered);
+    // console.log(ordered);
+    let v = compute(ordered, i);
+    filtered = filter(filtered, v, i);
+    i++;
+  }
+
+  // console.log(filtered);
+  return filtered;
+};
 
 const example = async () => {
-  const values = await read("./data.txt", "utf8").then((data) =>
+  const values = await read("./example.txt", "utf8").then((data) =>
     data.split("\n")
   );
 
-  const result = stepOne(values);
-  console.log("example result =", result);
+  const a = process(values, (ordered, i) =>
+    ordered[i]["0"] <= ordered[i]["1"] ? "1" : "0"
+  );
+  const b = process(values, (ordered, i) =>
+    ordered[i]["0"] <= ordered[i]["1"] ? "0" : "1"
+  );
+  console.log("example result =", a, b);
+  return parseInt(a, 2) * parseInt(b, 2);
 };
 
 const run = async () => {
@@ -19,8 +59,14 @@ const run = async () => {
     data.split("\n")
   );
 
-  const result = stepOne(values);
-  console.log(`result =`, result);
+  const a = process(values, (ordered, i) =>
+    ordered[i]["0"] <= ordered[i]["1"] ? "1" : "0"
+  );
+  const b = process(values, (ordered, i) =>
+    ordered[i]["0"] <= ordered[i]["1"] ? "0" : "1"
+  );
+  console.log("example result =", a, b, parseInt(a, 2) * parseInt(b, 2));
+  return parseInt(a, 2) * parseInt(b, 2);
 };
 
 example();
