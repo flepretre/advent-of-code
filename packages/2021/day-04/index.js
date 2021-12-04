4b6432b7279446caf838a0feebbd2c0bfb1f4291
Florent Lepretre <florent.lepretre@gmail.com>
2021-12-04 13:09:03 +0100

    day 4

--- packages/2021/day-04/index.js
+++ packages/2021/day-04/index.js
@@ -1,16 +1,83 @@
 const fs = require("fs");
 const util = require("util");
+const _ = require("lodash");
 
 const read = util.promisify(fs.readFile);
 
-const process = (values) => {};
+const process = (values) => {
+  let [numbers, ...grids] = values;
+  numbers = numbers.split(",").map((v) => parseInt(v));
+  grids = grids
+    .reduce((acc, line) => {
+      if (line === "") {
+        acc.push({ currentLine: 0 });
+        return acc;
+      }
+
+      const currentGrid = acc[acc.length - 1];
+      line
+        .split(" ")
+        .filter((v) => v)
+        .reduce((acc, v, i) => {
+          acc[v] = { value: parseInt(v), x: i, y: currentGrid.currentLine };
+          return acc;
+        }, currentGrid);
+      currentGrid.currentLine++;
+      return acc;
+    }, [])
+    .map((g) => {
+      delete g.currentLine;
+      return g;
+    });
+
+  const gridResult = grids.map(() => ({
+    x: [0, 0, 0, 0, 0],
+    y: [0, 0, 0, 0, 0],
+  }));
+
+  let winner = false;
+  numbers.forEach((n) => {
+    if (winner) return;
+    grids.forEach((grid, gi) => {
+      if (winner) return;
+      if (typeof grid[n] !== "undefined") {
+        const { x, y } = grid[n];
+        grid[n].marked = true;
+        console.log(gi, x, y);
+        const result = gridResult[gi];
+        result.x[x]++;
+        result.y[y]++;
+        if (result.x[x] > 4) {
+          console.log("winner grid", gi, "with colum", x, "and number", n);
+          winner = { grid: gi, number: n };
+          return;
+        }
+        if (result.y[y] > 4) {
+          console.log("winner grid", gi, "with line", y, "and number", n);
+          winner = { grid: gi, number: n };
+          return;
+        }
+      }
+    });
+  });
+
+  let winningGrid = grids[winner.grid];
+  console.log(winner, winningGrid);
+  const result = _.reduce(
+    winningGrid,
+    (acc, { value, marked }) => (marked ? acc : acc + value),
+    0
+  );
+
+  return result * winner.number;
+};
 
 const example = async () => {
   const values = await read("./example.txt", "utf8").then((data) =>
     data.split("\n")
   );
 
-  const result = process();
+  const result = process(values);
   console.log("example result =", result);
 };
 
@@ -19,7 +86,7 @@ const run = async () => {
     data.split("\n")
   );
 
-  const result = process();
+  const result = process(values);
   console.log(`result =`, result);
 };
 
--- packages/2021/day-04/part2.js
+++ packages/2021/day-04/part2.js
@@ -1,16 +1,89 @@
 const fs = require("fs");
 const util = require("util");
+const _ = require("lodash");
 
 const read = util.promisify(fs.readFile);
 
-const process = (values) => {};
+const process = (values) => {
+  let [numbers, ...grids] = values;
+  numbers = numbers.split(",").map((v) => parseInt(v));
+  grids = grids
+    .reduce((acc, line) => {
+      if (line === "") {
+        acc.push({ currentLine: 0 });
+        return acc;
+      }
+
+      const currentGrid = acc[acc.length - 1];
+      line
+        .split(" ")
+        .filter((v) => v)
+        .reduce((acc, v, i) => {
+          acc[v] = { value: parseInt(v), x: i, y: currentGrid.currentLine };
+          return acc;
+        }, currentGrid);
+      currentGrid.currentLine++;
+      return acc;
+    }, [])
+    .map((g) => {
+      delete g.currentLine;
+      return g;
+    });
+
+  let gridResult = grids.map((v, i) => ({
+    x: [0, 0, 0, 0, 0],
+    y: [0, 0, 0, 0, 0],
+    index: i,
+    winOrder: null,
+    winningNumber: null,
+  }));
+
+  let winOrder = 0;
+  numbers.forEach((n) => {
+    grids.forEach((grid, gi) => {
+      if (typeof grid[n] !== "undefined") {
+        const { x, y } = grid[n];
+        console.log(gi, x, y);
+        const result = gridResult[gi];
+        if (result.winOrder !== null) return;
+
+        grid[n].marked = true;
+        result.x[x]++;
+        result.y[y]++;
+        if (result.x[x] > 4) {
+          console.log("winner grid", gi, "with colum", x, "and number", n);
+          result.winOrder = winOrder++;
+          result.winningNumber = n;
+          return;
+        }
+        if (result.y[y] > 4) {
+          console.log("winner grid", gi, "with line", y, "and number", n);
+          result.winOrder = winOrder++;
+          result.winningNumber = n;
+          return;
+        }
+      }
+    });
+  });
+
+  gridResult = gridResult.sort(({ winOrder: a }, { winOrder: b }) => b - a);
+  let { index, winningNumber } = _.first(gridResult);
+  let winningGrid = grids[index];
+  const result = _.reduce(
+    winningGrid,
+    (acc, { value, marked }) => (marked ? acc : acc + value),
+    0
+  );
+
+  return result * winningNumber;
+};
 
 const example = async () => {
   const values = await read("./example.txt", "utf8").then((data) =>
     data.split("\n")
   );
 
-  const result = process();
+  const result = process(values);
   console.log("example result =", result);
 };
 
@@ -19,7 +92,7 @@ const run = async () => {
     data.split("\n")
   );
 
-  const result = process();
+  const result = process(values);
   console.log(`result =`, result);
 };
 
